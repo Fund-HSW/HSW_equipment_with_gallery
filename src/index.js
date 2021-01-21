7f8169a86c96635fd6d8c5fc99d31b5c4bfd9a93
Marcin Prokop <marcinprokop6@gmail.com>
2021-01-21 02:57:46 +0100

    rewriting most of the code

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-new Galery("heavyGlovesInfo", [
+monitorScreenSize();
+
+addGallery("heavyGlovesInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_1-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_2-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_3-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_4-scaled.jpg"
 ]);
-new Galery("plastronInfo", [
+addGallery("plastronInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/plastron_1-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/underplastron_1-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/underplastron_2-scaled.jpg"
 ]);
 
-new Galery("jacketInfo", [
+addGallery("jacketInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/przeszywanica_1-scaled.jpg"
 ]);
 
-new Galery("miscInfo", [
+addGallery("miscInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/plecy_1-scaled.jpg"
 ]);
-new Galery("jointInfo", [
+addGallery("jointInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nalokietniki_2-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nalokietniki_1-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nakolanniki_1-scaled.jpg"
 ]);
-new Galery("hoodInfo", [
+addGallery("hoodInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nakladka_1-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nakladka_2-scaled.jpg",
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/nakladka_3-scaled.jpg"
 ]);
-new Galery("maskInfo", [
+addGallery("maskInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/maska_1-scaled.jpg"
 ]);
-new Galery("headPaddingInfo", [
+addGallery("headPaddingInfo", [
   "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/czepek_1-scaled.jpg"
 ]);
--- src/source.js
+++ src/source.js
@@ -1,22 +1,55 @@
-const createGallery = () => {
-    let createdGallery = document.createElement("div");
-    createdGallery.setAttribute("class", "photo_gallery");
-    return createdGallery
+//small helper
+const logThis = (label, info) => {
+  console.log(`%c${label}: %c${info}`,"color:#42826D; font-weight:bold", "color:gray; font-style: italic")
 };
-/*
-  const createIllustration = (source, imageWidth, counterValue) => {
-    let createdIllustration = document.createElement("img");
-    createdIllustration.setAttribute("src", source);
-    if (imageWidth !== null) 
-      {createdIllustration.style.maxWidth = `${imageWidth}%` }
-    return createdIllustration
-};
-*/
-class Picture {
-  constructor (source, imageWidth, counterValue) {
-    let picture = document.createElement("img");
+const createTag = (tag) => document.createElement(`${tag}`);
 
-    picture.src = source;
+const testPhoto = [
+  "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_1-scaled.jpg",
+  "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_2-scaled.jpg",
+  "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_3-scaled.jpg",
+  "https://historycznesztukiwalki.pl/wp-content/uploads/2020/12/rekawice_4-scaled.jpg"
+];
+
+const increment = (current, max) => {
+  return current >= max-1 ? 0 : current+1
+};
+const decrement = (current, max) => {
+  return current <= 0 ? max-1 : current-1
+};
+
+const checkScreenSize = () => {
+  return (document.body.clientWidth > 1000) ? true : false
+};
+
+let isBigscreen = checkScreenSize();
+
+const monitorScreenSize = () => {
+  window.addEventListener('resize', () => {isBigscreen = checkScreenSize(), logThis("current screen size is:", document.body.clientWidth), logThis("screen considerd big?", isBigscreen)})
+};
+
+const calcPhotoWidth = (photoAmount) => {
+  return Math.ceil(100/photoAmount)-1
+}
+
+class EmptyGallery {
+  constructor(photoList, currentPhotoID) {
+    const emptyGallery = createTag("div");
+    
+    emptyGallery.className="photo_gallery";
+    emptyGallery.photoList = photoList;
+    emptyGallery.currentPhotoID=currentPhotoID;
+    emptyGallery.gallerySize = photoList.length;
+    
+    return emptyGallery;
+  }
+};
+
+class Picture {
+  constructor (photoSrc, imageWidth) {
+    let picture = createTag("img");
+
+    picture.src = photoSrc;
     picture.style.maxWidth = `${imageWidth}%` 
 
     return picture
@@ -26,32 +59,15 @@ class Picture {
 class Counter {
   constructor (currentIndex,maxIndex){
 
-    let counterBox = document.createElement("div")
-    let counter = document.createElement("p")
-    counterBox.append(counter)
+    let counter = document.createElement("div");
+    counter.className="counter"
+    counter.append(document.createElement("p"));
+    counter.children[0].innerText= `(${currentIndex+1}/${maxIndex})`;
 
-    counter.innerText= `${currentIndex}/${maxIndex}`
-
-    return counterBox
+    return counter
   }
 }
 /*
-  const createFigure = () => {
-    let createdFigure = document.createElement("div");
-    createdFigure.setAttribute("class", "figure");
-    createdFigure.currentIndex = 0;
-    return createdFigure
-};
-*/
-class Figure {
-  constructor () {
-    let figure = document.createElement("div")
-    figure.className = "figure"
-    figure.currentIndex = 0
-    return figure
-  }
-}
-//work in progress
 class Modal {
   constructor () {
     let modal = document.createElement("div")
@@ -60,7 +76,7 @@ class Modal {
     let modalContent = document.createElement("div")
     modal.className = "modal_content"
 
-  <!-- Modal content -->
+  <-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
     <p>Some text in the Modal..</p>
@@ -69,8 +85,8 @@ class Modal {
 </div>
   }
 }
-
-
+*/
+/* do przepisania 
 const preparePhotoData = (step,sourceData) => {
     let prepearedPhotoData = {}
     prepearedPhotoData.illustration = createIllustration(sourceData[step])
@@ -118,15 +134,122 @@ const smallScreenGalery = (id, photoArray) => {
         figure.children[1] = photoData.counter
         gallery.children[0] = figure
         insertionSite.children[0] = gallery
-        */
+        
        console.log("CLICK! CLICK ;/")
     };
     
         figure.onclick = (() => loadNext());
 };
+*/
 
-class Galery {
-  constructor(id, listOfPictures) {
-    window.inderWidth > 500 ? bigScreenGalery(id, listOfPictures) : smallScreenGalery(id, listOfPictures)
+const createGalleryDependingOnScreensize = (photoList, gallerySize) => {
+  return isBigscreen ? 
+  (logThis("making big gallery", `(${document.body.clientWidth})`), 
+  new BigScreenGalery(photoList, calcPhotoWidth(gallerySize))) : 
+  (logThis("making small gallery",`(${document.body.clientWidth})`),
+  new SmallScreenGalery(photoList, 0, gallerySize, "100")
+  );
+}
+
+class Gallery {
+  constructor(hook, photoList) {
+    logThis("make gallery for", hook)
+
+    let gallery = new EmptyGallery(photoList);
+    document.querySelector(`#${hook}`).append(gallery);
+    
+    gallery.isWidescreenGallery = isBigscreen;
+    logThis("is this gallery big?", gallery.isWidescreenGallery)
+
+    gallery.content = createGalleryDependingOnScreensize(gallery.photoList,gallery.gallerySize);
+
+    gallery.append(gallery.content)
+
+    return gallery
   }
+};
+
+class SmallScreenGalery {
+  constructor(photoSrc, currentID, maxID, imageWidth) {
+  const sliderGallery = new Slider(photoSrc, currentID, maxID, imageWidth)
+  sliderGallery.style.maxHeight = `${document.body.clientWidth}px`;
+  
+  return sliderGallery
+  }
+};
+
+class Interface {
+  constructor(currentID, maxID) {
+    const intf = createTag("div");
+    intf.className ="interface"
+    
+    if (maxID > 1) {
+    const incrementingBtn = createTag("a")
+    incrementingBtn.href = "javascript:;"
+    incrementingBtn.textContent = ">";
+
+    const decrementingBtn = createTag("a")
+    decrementingBtn.href = "javascript:;"
+    decrementingBtn.textContent = "<";
+
+    intf.append(decrementingBtn);
+    intf.append(new Counter(currentID, maxID));
+    intf.append(incrementingBtn);
+
+    incrementingBtn.addEventListener(
+      "click", 
+      () => {currentID = increment(currentID, maxID), logThis("Click!", currentID)});
+
+    decrementingBtn.addEventListener(
+       "click", 
+      () => {currentID = decrement(currentID, maxID), logThis("Click!", currentID)});
+    };
+
+  return intf;
+  };
+};
+
+class Slider {
+  constructor(photoSrc, currentID, maxID, imageWidth) {
+    const slider = createTag('div');
+    slider.className = "slider";
+
+    slider.append(new Picture (photoSrc[currentID], imageWidth));
+    slider.append(new Interface(currentID, maxID))
+
+    return slider
+  }
+};
+
+class BigScreenGalery {
+  constructor(photoSrc, imageWidth) {
+    const gallery = createTag("div")
+    gallery.className="big_gallery"
+    const photos = photoSrc.map(photo => new Picture(photo, imageWidth));
+
+    photos.forEach(photo => gallery.append(photo));
+
+    return gallery    
+}
+}; 
+
+const handleWindowSizeChange = (hook, gallery) => {
+  gallery = document.querySelector(`#${hook} .photo_gallery`)
+  gallery.content = createGalleryDependingOnScreensize(gallery.photoList,gallery.gallerySize)
+
+  gallery.replaceChildren(gallery.content, gallery.children[0])
+  gallery.isWidescreenGallery = isBigscreen
+};
+
+const addGallery = (hook, photoList) => {
+  let gallery = new Gallery(hook, photoList);
+  
+  window.addEventListener('resize', ()=>{
+    if (gallery.isWidescreenGallery !== isBigscreen) {
+      handleWindowSizeChange(hook, gallery)
+    }
+  }
+  )
+
+  return gallery
 };
